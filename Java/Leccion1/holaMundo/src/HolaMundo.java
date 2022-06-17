5efef183800297c10218bfe84506bc212178dd6f
agustin2589 <70908351+rone2589@users.noreply.github.com>
2022-06-17 00:17:39 -0300

    Apuntes y ejercicios Java clase 4 - Tipos primitivos

--- Java/Leccion1/holaMundo/src/HolaMundo.java
+++ Java/Leccion1/holaMundo/src/HolaMundo.java
@@ -59,6 +59,7 @@ public class HolaMundo {
         */
         
         //Clase Scanner
+        /*
         Scanner entrada = new Scanner(System.in);
         System.out.println("Ingrese un nombre: ");
         var usuario2 = entrada.nextLine();
@@ -66,6 +67,38 @@ public class HolaMundo {
         System.out.println("Ingrese un numero: ");
         var numero = entrada.nextInt();
         System.out.println("numero: " + numero);
+        */
+        /*
+        byte numEnteroByte = (byte)129;
+        System.out.println("numEnteroByte= "+ numEnteroByte);
+        System.out.println("Valor minimo del byte: "+ Byte.MIN_VALUE);
+        System.out.println("Valor maximo del byte: "+ Byte.MAX_VALUE);
+        
+        short numEnteroShort = (short)32768;
+        System.out.println("numEnteroShort= "+ numEnteroShort);
+        System.out.println("Valor minimo del short: "+ Short.MIN_VALUE);
+        System.out.println("Valor maximo del short: "+ Short.MAX_VALUE);
+        
+        int numEntero = (int)2147483648L;
+        System.out.println("numEntero= "+ numEntero);
+        System.out.println("Valor minimo del int: "+ Integer.MIN_VALUE);
+        System.out.println("Valor maximo del int: "+ Integer.MAX_VALUE);
+        
+        long numEnteroLong = (long)9223372036854775807L;
+        System.out.println("numEnteroLong= "+ numEnteroLong);
+        System.out.println("Valor minimo del Long: "+ Long.MIN_VALUE);
+        System.out.println("Valor maximo del Long: "+ Long.MAX_VALUE);
+        */
+        
+        float numFloat = 3.4028235E38F;
+        System.out.println("numFloat = "+ numFloat);
+        System.out.println("El valor minimo de float: " + Float.MIN_VALUE);
+        System.out.println("El valor maximo de float: " + Float.MAX_VALUE);
+        
+        double numDouble = 1.7976931348623157E308;
+        System.out.println("numDouble = "+ numDouble);
+        System.out.println("El valor minimo de double: " + Double.MIN_VALUE);
+        System.out.println("El valor maximo de double: " + Double.MAX_VALUE);
         
         /*
         System.out.println(saludo.equals(saludo2));//retorna false
