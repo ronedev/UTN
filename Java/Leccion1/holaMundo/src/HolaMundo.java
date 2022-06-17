f8baa33479957691b641246cdee2a86f3a1d865d
agustin2589 <70908351+rone2589@users.noreply.github.com>
2022-06-16 21:28:09 -0300

    Apuntes Java clase 2 - Variables, enteros y string

--- Java/Leccion1/holaMundo/src/HolaMundo.java
+++ Java/Leccion1/holaMundo/src/HolaMundo.java
@@ -11,6 +11,18 @@ public class HolaMundo {
         String saludo2 = "Hola manola";
         //Para comparar string se utiliza equals, no ===
         
+        int miVariableReutilizable = 10;
+        System.out.println(miVariableReutilizable);
+        miVariableReutilizable = 5;
+        System.out.println(miVariableReutilizable);
+        
+        //Tipo string
+        String miVariableString = "holitaaa";
+        System.out.println(miVariableString);
+        
+        miVariableString = "otra cadena";
+
+        
         System.out.println(saludo.equals(saludo2));//retorna false
         
         System.out.println(saludo + saludo2); //Cuando se suman string se concatena
