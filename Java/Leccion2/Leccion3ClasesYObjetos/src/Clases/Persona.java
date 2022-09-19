3ac8780a5653a52239de33c5182ff91f0969253e
agustin2589 <70908351+rone2589@users.noreply.github.com>
2022-09-19 16:45:12 -0300

    add: Clases, atributos y objetos en Java - Clase 4

--- Java/Leccion2/Leccion3ClasesYObjetos/src/Clases/Persona.java
+++ Java/Leccion2/Leccion3ClasesYObjetos/src/Clases/Persona.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Clases;
 
-/**
- *
- * @author WIN-10
- */
 public class Persona {
-    
+   String name;
+   String lastname;
+   
+   public void showNames(){
+       System.out.println("name = " + name);
+       System.out.println("lastname = " + lastname);
+   }
 }
---
+++ Java/Leccion2/Leccion3ClasesYObjetos/src/Clases/PruebaPersona.java
@@ -0,0 +1,12 @@
+
+package Clases;
+
+public class PruebaPersona {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+        
+        persona1.name = "Agustin";
+        persona1.lastname = "Vera";
+        persona1.showNames();
+    }
+}
