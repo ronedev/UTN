2424e6193c715d21852dcc725b2544ff96007c4f
agustin2589 <70908351+rone2589@users.noreply.github.com>
2022-09-30 16:47:47 -0300

    add: Clase 5 Programacion con Java, Clases y objetos parte 3

--- Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
+++ Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -10,4 +10,16 @@ public class Aritmetica {
         int resultado = a + b;
         System.out.println("resultado = " + resultado);
     }
+    
+    public int sumarConRetorno(){
+        int resultado = a + b;
+        return resultado;
+    }
+    
+    public int sumarConArgumentos(int num1, int num2){
+        a = num1;
+        b = num2;
+        //return a + b;
+        return sumarConRetorno();
+    }
 }
