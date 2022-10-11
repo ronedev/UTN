6c357aa4104af73cdf832c8b11070313a516bd9d
agustin2589 <70908351+rone2589@users.noreply.github.com>
2022-10-11 17:14:38 -0300

    add: Clase 4 Javascript Tipos de Datos parte 2 - Laboratorio+

--- Javascript/Leccion02/02-01-TiposDatos.js
+++ Javascript/Leccion02/02-01-TiposDatos.js
@@ -4,7 +4,9 @@ La sintaxis en lo que es comentarios es muy similar a la de Java
 */
 
 var nombre = "Agustin" //Tipo string
-console.log(nombre)
+console.log(typeof nombre)
+nombre = 7
+console.log(typeof nombre);
 
 var num = 16 //Tipo numerico
 console.log(num);
@@ -14,3 +16,25 @@ var objeto = {
     surname: 'Bentancud'
 }
 console.log(objeto)
+
+//Tipo de dato boolean
+var bandera = true
+console.log(bandera)
+
+//Tipo de dato funcion
+function miFuncion(){}
+console.log(typeof miFuncion);
+
+//Tipo de dato symbol
+var simbolo = Symbol("Mi simbolo")
+console.log(simbolo)
+
+//Tipo de dato clase
+class Persona{
+    constructor(name, surname){
+        this.name = name
+        this.surname = surname
+    }
+}
+
+console.log(new Persona('Agustin', 'Vera'));
