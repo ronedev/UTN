33492ea3a24d1fb3f62402d0d6ad8d5f89b88168
agustin2589 <70908351+rone2589@users.noreply.github.com>
2022-10-11 17:06:56 -0300

    add: Clase 3 Javascript Tipos de Datos - Laboratorio

---
+++ Javascript/Leccion02/02-01-TiposDatos.js
@@ -0,0 +1,16 @@
+//Tipos de Datos en Javascript
+/*
+La sintaxis en lo que es comentarios es muy similar a la de Java
+*/
+
+var nombre = "Agustin" //Tipo string
+console.log(nombre)
+
+var num = 16 //Tipo numerico
+console.log(num);
+
+var objeto = {
+    name: 'Ariel',
+    surname: 'Bentancud'
+}
+console.log(objeto)
