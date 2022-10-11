9b8e2df5e78848037c11f40bcb5730b5f6924a58
agustin2589 <70908351+rone2589@users.noreply.github.com>
2022-10-11 16:56:50 -0300

    add: Clase 2 Javascript HolaMundo - Laboratorio

---
+++ Javascript/Leccion01/01-01-holamundo.js
@@ -0,0 +1,4 @@
+var name = "Agustin";
+name = "Gaston \nAgustin\n"
+var surname = "Vera"
+console.log(name+surname)
---
+++ Javascript/Leccion01/HolaMundo.js
@@ -0,0 +1,4 @@
+var name = "Agustin"
+name = "Hola Mundo desde JavaScript"
+
+console.log(name);
