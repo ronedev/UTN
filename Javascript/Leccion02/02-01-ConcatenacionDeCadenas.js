992fed314d47d32690513b3bc95a60ef7b6dd1b0
agustin2589 <70908351+rone2589@users.noreply.github.com>
2022-10-25 16:41:08 -0300

    add: Clase 6 Concatenar cadenas

---
+++ Javascript/Leccion02/02-01-ConcatenacionDeCadenas.js
@@ -0,0 +1,16 @@
+var nombre = "Agustin"
+var apellido = "Vera"
+var nombreCompleto = nombre + ' ' + apellido
+
+console.log(nombreCompleto);
+var nombreCompleto2 = 'Ariel' + ' ' + 'Bentancud'
+console.log(nombreCompleto2);
+
+var juntos = nombre + 234
+console.log(juntos)
+juntos = nombre + 78 + 17
+console.log(juntos);
+juntos = 78 + 17 + nombre
+console.log(juntos)
+juntos = 78 + (17 + nombre)
+console.log(juntos)
