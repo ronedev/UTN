5efbe7a0a3cd4b3dd873dcb9b6453e2a6f630afc
Agustin notebook <ronedev25@gmail.com>
2022-11-03 08:11:16 -0300

    add: Clase 7 de Javascript - Similitudes con Java

--- Javascript/Leccion02/02-01-ConcatenacionDeCadenas.js
+++ Javascript/Leccion02/02-01-ConcatenacionDeCadenas.js
@@ -14,3 +14,14 @@ juntos = 78 + 17 + nombre
 console.log(juntos)
 juntos = 78 + (17 + nombre)
 console.log(juntos)
+
+let x, y //Se pueden crear varias variables en la misma linea
+x = 17, y = 21 //Se puede hacer asignacion de varias variables en la misma linea
+let z = x + y //Se asigna el valor de la operacion
+console.log('z: ',z)
+
+let _1num = 16 //No usar numeros para iniciar nombre de variable
+let rompiendo = 'break' //no usar palabras reservadas como nombre de variable
+
+console.log(_1num)
+console.log(rompiendo)
---
+++ Javascript/Leccion03/03-01-OperadoresJavascript.js
@@ -0,0 +1,16 @@
+//Ejercicio para encontrar numeros pares
+let num = 5
+
+if(num % 2 == 0){
+    console.log('El numero es par')
+}else{
+    console.log('El numero es impar')
+}
+
+//Ejercicio es mayor de edad
+let edad = 35
+if(edad >= 18){
+    console.log('Es mayor de edad')
+}else{
+    console.log('Es menor de edad')
+}
