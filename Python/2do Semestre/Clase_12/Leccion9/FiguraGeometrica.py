bdbb559fb265ce4d9dcb2a427b00113564720249
Agustin notebook <ronedev25@gmail.com>
2022-11-09 18:11:49 -0300

    add: Clase 13 Laboratorio con Python - Variables de clase, clases abstractas y atributos de solo lectura

--- Python/2do Semestre/Clase_12/Leccion9/FiguraGeometrica.py
+++ Python/2do Semestre/Clase_12/Leccion9/FiguraGeometrica.py
@@ -1,15 +1,28 @@
-class FiguraGeometrica:
+from abc import ABC, abstractmethod
+#ABC significa Abstract Base Class, convierte una clase a abstracta
+class FiguraGeometrica(ABC):
     def __init__(self, ancho, alto):
-        self._alto = alto
-        self._ancho = ancho
-    
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            self._alto = 0
+            print(f'{alto} es un valor erroneo')
+        if self._validar_valores(ancho):
+            self._ancho = ancho
+        else:
+            self._ancho = 0
+            print(f'{alto} es un valor erroneo')
+
     @property
     def alto(self):
         return self._alto
     
     @alto.setter
     def alto(self, alto):
-        self._alto = alto
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            print(f'Valor erroneo para el alto: {alto}')
 
     @property
     def ancho(self):
@@ -17,7 +30,17 @@ class FiguraGeometrica:
     
     @ancho.setter
     def ancho(self, ancho):
-        self._ancho = ancho
+        if self._validar_valores(ancho):
+            self._ancho = ancho
+        else:
+            print(f'Valor erroneo para el ancho: {ancho}')
+    
+    @abstractmethod
+    def calcularArea(self):
+        pass
     
     def __str__(self):
         return f'FiguraGeometrica: [ancho: {self._ancho}, alto: {self._alto}]'
+    
+    def _validar_valores(self, valor):
+        return True if valor < 10 and valor > 0 else False
--- Python/2do Semestre/Clase_12/Leccion9/testFiguraGeometrica.py
+++ Python/2do Semestre/Clase_12/Leccion9/testFiguraGeometrica.py
@@ -1,5 +1,6 @@
 from Cuadrado import Cuadrado
 from Rectangulo import Rectangulo
+from FiguraGeometrica import FiguraGeometrica
 
 cuadrado1 = Cuadrado(5, 'rojo')
 print(cuadrado1.ancho)
@@ -8,8 +9,11 @@ print(cuadrado1)
 print(f'El area del cuadrado 1 es: {cuadrado1.calcularArea()}')
 
 #Test rectangulo
-rectangulo1 = Rectangulo(5, 10, 'verde')
+rectangulo1 = Rectangulo(5, 8, 'verde')
 print(rectangulo1.ancho)
 print(rectangulo1.alto)
 print(rectangulo1)
 print(f'El area del rectangulo 1 es: {rectangulo1.calcularArea()}')
+
+#Test clase abstracta
+#figura1 = FiguraGeometrica() #No se puede instanciar porque es abstracta
