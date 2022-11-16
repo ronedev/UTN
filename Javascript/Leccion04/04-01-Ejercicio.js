5823946958656c1fa5cc28ae41563e78864d1aab
Agustin notebook <ronedev25@gmail.com>
2022-11-16 10:15:01 -0300

    add: Clase 9 de Laboratorio con JavaScript - Operadores y funciones isNaN y Number

--- Javascript/Leccion04/04-01-Ejercicio.js
+++ Javascript/Leccion04/04-01-Ejercicio.js
@@ -49,3 +49,41 @@ const añoNacimiento = 1999
 console.log(añoNacimiento)
 // añoNacimiento = 2015
 // console.log(añoNacimiento)
+
+papa_vacaciones = false
+papa_dia_de_descanso = true
+if(papa_vacaciones || papa_dia_de_descanso){
+    console.log("Puede ir al partido")
+}else{
+    console.log("No puede ir al partido")
+}
+
+//Operador ternario
+let resultado = papa_dia_de_descanso || papa_vacaciones ? 'Puede ir al partido' : 'No puede ir al partido'
+console.log(resultado)
+
+let numero = 10
+let resultado2 = numero % 2 === 0 ? 'El numero es Par' : 'El numero es impar'
+console.log(resultado2)
+
+//Convertir String a Number
+let miNumero = '20'
+console.log(typeof miNumero)
+let edad2 = Number(miNumero)
+console.log(edad2)
+console.log(typeof edad2)
+
+if(isNaN(edad2)){
+    console.log("No contiene unicamente numeros")
+}else{
+    if(edad2 >= 18){
+        console.log('Es Mayor de edad')
+    }else{
+        console.log('Es menor de edad')
+    }    
+}
+
+let resultado3 = edad2 >= 18 ? "Es mayor de edad": "Es menor de edad"
+console.log(resultado3)
+
+//Funcion isNaN
